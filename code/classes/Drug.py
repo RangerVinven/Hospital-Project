a914eef273699c5b6d9cd174854455d134aa6df5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-29 17:35:30 +0000

    Added comments

--- code/classes/Drug.py
+++ code/classes/Drug.py
@@ -9,9 +9,11 @@ class Drug():
 
         self.create_drug(database_connector)
 
+    # Saves the drug to the database
     def create_drug(self, database_connector):
+        # Gets the ID
         id = IDGenerator.generate_id(db_connector=database_connector, id_length=2, hasNumbers=False, hasLetters=True, table="Drug", id_column_name="drugID")
-        print(id)
 
+        # Saves the drug to the database
         database_connector.cursor.execute("INSERT INTO Drug (drugID, name, sideeffects, benefits) VALUES (%s, %s, %s, %s);", (id, self.name, self.side_effects, self.benefits))
         database_connector.db.commit()
