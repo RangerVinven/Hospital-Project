5c56b4dbf5ff718a06de7571ee45a4bc5ef3fc24
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-29 17:37:38 +0000

    Moved id generation to __init__()

--- code/classes/Drug.py
+++ code/classes/Drug.py
@@ -2,7 +2,7 @@ from utils.id_generator import IDGenerator
 
 class Drug():
     def __init__(self, database_connector, name, side_effects, benefits):
-        self.drug_id = 0
+        self.drug_id = IDGenerator.generate_id(db_connector=database_connector, id_length=2, hasNumbers=False, hasLetters=True, table="Drug", id_column_name="drugID")
         self.name = name
         self.side_effects = side_effects
         self.benefits = benefits
@@ -11,9 +11,6 @@ class Drug():
 
     # Saves the drug to the database
     def _create_drug(self, database_connector):
-        # Gets the ID
-        id = IDGenerator.generate_id(db_connector=database_connector, id_length=2, hasNumbers=False, hasLetters=True, table="Drug", id_column_name="drugID")
-
         # Saves the drug to the database
-        database_connector.cursor.execute("INSERT INTO Drug (drugID, name, sideeffects, benefits) VALUES (%s, %s, %s, %s);", (id, self.name, self.side_effects, self.benefits))
+        database_connector.cursor.execute("INSERT INTO Drug (drugID, name, sideeffects, benefits) VALUES (%s, %s, %s, %s);", (self.drug_id, self.name, self.side_effects, self.benefits))
         database_connector.db.commit()
