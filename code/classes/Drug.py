d4c27446b62f7b9e7a4773232045afc500d6b0a5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-29 15:30:09 +0000

    Created Drug classes

---
+++ code/classes/Drug.py
@@ -0,0 +1,6 @@
+class Drug():
+    def __init__(self, name, side_effects, benefits):
+        self.drug_id = 0
+        self.name = name
+        self.side_effects = side_effects
+        self.benefits = benefits
