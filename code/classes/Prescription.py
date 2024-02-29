f334ffa5a1532b78366db8d2c57c52ba26a7f188
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-29 15:30:30 +0000

    Created Prescription classes

---
+++ code/classes/Prescription.py
@@ -0,0 +1,6 @@
+class Prescription():
+    def __init__(self, date_of_prescription, dosage, duration, comment):
+        self.date_of_prescription = date_of_prescription
+        self.dosage = dosage
+        self.duration = duration
+        self.comments = comment
