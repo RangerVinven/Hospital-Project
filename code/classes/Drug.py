c0020865bbcc250a25d4eff69458b14d5e66d63c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-29 17:32:30 +0000

    Created create_drug method

--- code/classes/Drug.py
+++ code/classes/Drug.py
@@ -1,6 +1,17 @@
+from utils.id_generator import IDGenerator
+
 class Drug():
-    def __init__(self, name, side_effects, benefits):
+    def __init__(self, database_connector, name, side_effects, benefits):
         self.drug_id = 0
         self.name = name
         self.side_effects = side_effects
         self.benefits = benefits
+
+        self.create_drug(database_connector)
+
+    def create_drug(self, database_connector):
+        id = IDGenerator.generate_id(db_connector=database_connector, id_length=2, hasNumbers=False, hasLetters=True, table="Drug", id_column_name="drugID")
+        print(id)
+
+        database_connector.cursor.execute("INSERT INTO Drug (drugID, name, sideeffects, benefits) VALUES (%s, %s, %s, %s);", (id, self.name, self.side_effects, self.benefits))
+        database_connector.db.commit()
