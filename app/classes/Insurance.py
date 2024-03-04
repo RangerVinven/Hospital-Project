da80e9386e60f9df5febbd1ac991232527f4c632
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-03-04 10:21:55 +0000

    Added the create and list methods

--- app/classes/Insurance.py
+++ app/classes/Insurance.py
@@ -1,6 +1,21 @@
+from utils.id_generator import IDGenerator
+
 class Insurance():
-    def __init__(self, insurance_id, company, address, phone):
-       self.insurance_id = insurance_id
+    def __init__(self, database_connector, company, address, phone):
+       self.insurance_id = IDGenerator.generate_id(database_connector, 7, True, True, "Insurance", "insuranceID")
        self.company = company
        self.address = address
-       self.phone = phone 
+       self.phone = phone
+
+       self._create_insurance(database_connector)
+
+    # Inserts the insurance into the table
+    def _create_insurance(self, database_connector):
+        database_connector.cursor.execute("INSERT INTO Insurance (insuranceID, company, address, phone) VALUES (%s, %s, %s, %s);", (self.insurance_id, self.company, self.address, self.phone))
+        database_connector.db.commit()
+
+    # Returns all the insurance companies
+    @staticmethod
+    def list_insurance(database_connector):
+        database_connector.cursor.execute("SELECT * FROM Insurance;")
+        return database_connector.cursor.fetchall()
