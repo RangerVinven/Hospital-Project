5afaacc7784e9caecefe2b6a9b3e0c136e0f12d9
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-03-08 18:12:32 +0000

    Changed create_update_query to work with composite keys

--- app/utils/query_builder.py
+++ app/utils/query_builder.py
@@ -19,10 +19,9 @@ class QueryBuilder():
         return query, variables
 
     @staticmethod
-    def create_update_query(table_name: str, table_id_name: str, table_id_value: str, column_names: tuple, column_values: tuple):
-        # Creates the initial query
-        query = "UPDATE {} SET {}=%s".format(table_name, table_id_name)
-        variables = [table_id_value]
+    def create_update_query(table_name: str, table_id_names: tuple, table_id_values: tuple, column_names: tuple, column_values: tuple):
+        query = "UPDATE {} SET {}=%s".format(table_name, table_id_names[0])
+        variables = [table_id_values[0]]
 
         # Adds each search parameters to the query
         # If the user puts "Patient", (name, address, email), ("John", "", "john@gmail.com")
@@ -32,7 +31,13 @@ class QueryBuilder():
                 query += ", {}=%s".format(column_names[i])
                 variables.append(column_values[i])
 
-        query += "WHERE {}=%s;".format(table_id_name)
-        variables.append(table_id_value)
+        query += "WHERE 1=1"
+
+        # Adds the ID (or IDs for composite keys) to the where clause
+        for i in range(len(table_id_names)):
+            query += " AND {}=%s".format(table_id_names[i])
+            variables.append(table_id_values[i])
+
+        query += ";"
 
         return query, variables
