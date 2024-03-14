021b099968f76585c10af641f8bde8fd7ebd4d63
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-03-14 22:16:15 +0000

    Moved the menu in a while True block

--- app/main.py
+++ app/main.py
@@ -1,31 +1,44 @@
 from menu.drug_menu import show_drug_menu
+from menu.visit_menu import show_visit_menu
+from menu.prescription_menu import show_prescription_menu
 from menu.insurance_menu import show_insurance_menu
 
 def show_menu():
-    main_menu = '''    
-1. Patients
-2. Doctors
-3. Drugs
-4. Prescription
-5. Visits
-6. Insurance
-99. Exit
-'''
-    print(main_menu)
-    
-    # Gets the selected option
-    selected_option = input("Enter which category you'd like to interact with: ")
 
-    while selected_option not in ["1", "2", "3", "4", "5", "6", "99"]:
-        print("Invalid option")
+    while True:
+        main_menu = '''    
+    1. Patients
+    2. Doctors
+    3. Drugs
+    4. Prescription
+    5. Visits
+    6. Insurance
+    99. Exit
+    '''
+        print(main_menu)
+        
+        # Gets the selected option
         selected_option = input("Enter which category you'd like to interact with: ")
 
-    # Shows the appropreate menu
-    if selected_option == "3":
-        show_drug_menu()
+        while selected_option not in ["1", "2", "3", "4", "5", "6", "99"]:
+            print("Invalid option")
+            selected_option = input("Enter which category you'd like to interact with: ")
 
-    elif selected_option == "6":
-        show_insurance_menu()
+        # Shows the appropreate menu
+        if selected_option == "3":
+            show_drug_menu()
+
+        elif selected_option == "4":
+            show_prescription_menu()
+
+        elif selected_option == "5":
+            show_visit_menu()
+
+        elif selected_option == "6":
+            show_insurance_menu()
+
+        else:
+            break
 
 
 if __name__ == "__main__":
