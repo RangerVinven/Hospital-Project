89fed85afb3bede2642d46be12d09a272752a4c1
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-04-02 18:41:42 +0100

    Created the CRUD methods

--- app/classes/Doctors.py
+++ app/classes/Doctors.py
@@ -1,11 +1,68 @@
+from utils.record_manager import RecordManager
+from utils.query_builder import QueryBuilder
+from utils.id_generator import IDGenerator
+
 class Doctor():
-    def __init__(self, firstname: str, surname: str, address: str, email: str):
-        self.doctor_id = 0  # TODO - add function to generate the ID
+    def __init__(self, database_connector, firstname: str, surname: str, address: str, email: str):
+        self.doctor_id = IDGenerator.generate_id(database_connector, 7, True, True, "Doctor", "doctorID")
         self.firstname = firstname
         self.surname = surname
         self.address = address
         self.email = email
 
+        self._create_doctor(database_connector)
+
+    def _create_doctor(self, database_connector):
+        database_connector.cursor.execute("INSERT INTO Doctors(doctorID, firstname, surname, address, email) VALUES (%s, %s, %s, %s, %s);", (self.doctor_id, self.firstname, self.surname, self.address, self.email))
+        database_connector.db.commit()
+
+    # Returns all the doctor companies
+    @staticmethod
+    def list_doctors(database_connector):
+        database_connector.cursor.execute("SELECT * FROM Doctor;")
+        RecordManager.print_records(database_connector.cursor.fetchall())
+
+    # Returns a specific doctor company details
+    @staticmethod
+    def find_doctor(database_connector):
+        # Gets the items the user wishes to change
+        id = input("Please enter the ID of the insurance you wish to search for (leave blank if unknown): ")
+        firstname = input("Please enter the firstname you wish to search for (leave blank if unknown): ")
+        surname = input("Please enter the surname you wish to search for (leave blank if unknown): ")
+        address = input("Please enter the address you wish to search for (leave blank if unknown): ")
+        email = input("Please enter the email you wish to search for (leave blank if unknown): ")
+
+        query, variables = QueryBuilder.create_find_query("Doctor", ("doctorID", "firstname", "surname", "address", "email"), (id, firstname, surname, address, email))
+
+        # Executes the query
+        database_connector.cursor.execute(query, tuple(variables))        
+        RecordManager.print_records(database_connector.cursor.fetchall())
+    
+    @staticmethod
+    def update_doctor(database_connector):
+
+        # Gets the new item details
+        doctor_id = input("Enter the ID of the doctor you want to update: ")
+        firstname = input("Enter the new firstname (leave blank for no change): ")
+        surname = input("Enter the new surname (leave blank for no change): ")
+        address = input("Enter the new address (leave blank for no change): ")
+        email = input("Enter the new email (leave blank for no change): ")
+
+        query, variables = QueryBuilder.create_update_query("Doctor", ("doctorID",), (doctor_id,), ("firstname", "surname", "address", "email"), (firstname, surname, address, email))
+
+        # Executes the query
+        database_connector.cursor.execute(query, tuple(variables))
+
+        RecordManager.is_row_changed(database_connector, "Doctor updated", "No doctor found, nothing was updated")
+        database_connector.db.commit()
+   
+    # Deletes a specific doctor
+    def delete_doctor(database_connector):
+        doctor_id = input("Please enter the id of the doctor you wish to delete: ")
+        database_connector.cursor.execute("DELETE FROM Doctor WHERE doctorID=%s", (doctor_id,))
+        
+        RecordManager.is_row_changed(database_connector, "Doctor deleted", "No doctor found, nothing was deleted")
+        database_connector.db.commit()
 
 class Specialist(Doctor):
     def __init__(self, firstname: str, surname: str, address: str, email: str, specilization: str, experience: int):
--- app/classes/Patients.py
+++ app/classes/Patients.py
@@ -1,12 +1,77 @@
+from utils.record_manager import RecordManager
+from utils.query_builder import QueryBuilder
+from utils.id_generator import IDGenerator
+
 class Patient():
-    def __init__(self, firstname, surname, postcode, address, phone, email):
-        self.patient_id = 0
+    def __init__(self, database_connector, firstname, surname, postcode, address, phone, email, insurance_ID):
+        self.patient_id = IDGenerator.generate_id(database_connector, 8, True, True, "Patient", "patientID")
         self.firstname = firstname
         self.surname = surname
         self.postcode = postcode
         self.address = address
         self.phone = phone
         self.email = email
+        self.insurance_id = insurance_ID
+
+        self._create_patient(database_connector)
+
+    def _create_patient(self, database_connector):
+        database_connector.cursor.execute("INSERT INTO Patient(patientID, firstname, surname, postcode, address, phone, email, insuranceID) VALUES (%s, %s, %s, %s, %s, %s);", (self.patient_id, self.firstname, self.surname, self.address, self.email, self.insurance_id))
+        database_connector.db.commit()
+
+    # Returns all the patient companies
+    @staticmethod
+    def list_patients(database_connector):
+        database_connector.cursor.execute("SELECT * FROM Patient;")
+        RecordManager.print_records(database_connector.cursor.fetchall())
+
+    # Returns a specific patient company details
+    @staticmethod
+    def find_patient(database_connector):
+        # Gets the items the user wishes to change
+        id = input("Please enter the ID of the patient you wish to search for (leave blank if unknown): ")
+        firstname = input("Please enter the firstname you wish to search for (leave blank if unknown): ")
+        surname = input("Please enter the surname you wish to search for (leave blank if unknown): ")
+        postcode = input("Please enter the postcode you wish to search for (leave blank if unknown): ")
+        address = input("Please enter the address you wish to search for (leave blank if unknown): ")
+        phone = input("Please enter the phone you wish to search for (leave blank if unknown): ")
+        email = input("Please enter the email you wish to search for (leave blank if unknown): ")
+        insurance_id = input("Please enter the insurance id you wish to search for (leave blank if unknown): ")
+
+        query, variables = QueryBuilder.create_find_query("Patient", ("patientID", "firstname", "surname", "postcode", "address", "phone", "email", "insuranceID"), (id, firstname, surname, postcode, address, phone, email, insurance_id))
+
+        # Executes the query
+        database_connector.cursor.execute(query, tuple(variables))        
+        RecordManager.print_records(database_connector.cursor.fetchall())
+    
+    @staticmethod
+    def update_patient(database_connector):
+
+        # Gets the new item details
+        patient_id = input("Enter the ID of the patient you want to update: ")
+        firstname = input("Enter the new firstname (leave blank for no change): ")
+        surname = input("Enter the new surname (leave blank for no change): ")
+        postcode = input("Enter the new postcode (leave blank for no change): ")
+        address = input("Enter the new address (leave blank for no change): ")
+        phone = input("Enter the new phone (leave blank for no change): ")
+        email = input("Enter the new email (leave blank for no change): ")
+        insurance_id = input("Enter the new insurance id (leave blank for no change): ")
+
+        query, variables = QueryBuilder.create_update_query("Patient", ("patientID",), (patient_id,), ("firstname", "surname", "postcode", "address", "phone", "email", "insurance_id"), (firstname, surname, postcode, address, phone, email, insurance_id))
+
+        # Executes the query
+        database_connector.cursor.execute(query, tuple(variables))
+
+        RecordManager.is_row_changed(database_connector, "Patient updated", "No patient found, nothing was updated")
+        database_connector.db.commit()
+   
+    # Deletes a specific patient
+    def delete_patient(database_connector):
+        patient_id = input("Please enter the id of the patient you wish to delete: ")
+        database_connector.cursor.execute("DELETE FROM Patient WHERE patientID=%s", (patient_id,))
+        
+        RecordManager.is_row_changed(database_connector, "Patient deleted", "No patient found, nothing was deleted")
+        database_connector.db.commit()
 
 
 class InsuredPatient(Patient):
