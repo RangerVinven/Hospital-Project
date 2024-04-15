31f41e7be227ac2a177ba1fcb0c3f83bab76e5d4
Daniel <daniel@daniels-MacBook-Pro.local>
2024-04-15 12:42:42 +0100

    Made changes to how it works in a container

--- app/utils/database_connector.py
+++ app/utils/database_connector.py
@@ -4,16 +4,15 @@ from time import sleep
 
 class DatabaseConnector():
     def __init__(self, database_to_connect_to: str):
-        
         # Initialises the variables
         self.username = "root"
         self.password = "root"
-        self.host = "127.0.0.1"
+        self.host = "172.20.0.2"
         self.database_name = "Hospital"
 
         # Connects to either the development or the testing database
         if database_to_connect_to == "development":
-            self.port = "3000"
+            self.port = "3306"
         
         else:
             self.port = "4000"
